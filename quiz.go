dbb6bee0d0c4f4eb42b116a342543c413cedead3
LinYujin <devishtank@gmail.com>
2023-03-06 11:40:51 +0700

    Part 1 of solution by calhoun

--- quiz.go
+++ quiz.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"os"
+	"flag"
+	"encoding/csv"
+	"fmt"
+	"strings"
 )
 
 func check(e error) {
@@ -11,7 +15,45 @@ func check(e error) {
 }
 
 func main(){
-	data,err := os.ReadFile("problems.csv")
+	csvfilename := flag.String("csv","problems.csv","CSV file which contains question and answer separated by comma")
+	flag.Parse()
+
+	file,err := os.Open(*csvfilename)
 	check(err)
-	println(string(data))
+	defer file.Close()
+
+	r := csv.NewReader(file)
+	lines,err := r.ReadAll()
+	check(err)
+
+	problems := parseLines(lines)
+
+	correct := 0
+	for i, p := range problems {
+		fmt.Printf("Problem #%d : %s\n", i+1, p.q)
+		var answer string
+		fmt.Scanf("%s\n",&answer)
+		if answer == p.a{
+			fmt.Printf("Correct\n")
+			correct++
+		}
+	}
+
+	fmt.Printf("You answered %d correct answer out of %d problems",correct, len(problems))
+}
+
+func parseLines(lines [][]string) []problem{
+	ret := make([]problem, len(lines))
+	for i, line := range lines {
+		ret[i] = problem{
+			q : line[0],
+			a : strings.TrimSpace(line[1]),
+		}
+	}
+	return ret
+}
+
+type problem struct{
+	q string
+	a string
 }
