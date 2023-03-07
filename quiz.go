aff56523c352d9819fb19d4f5ccbd5b2ada5fe52
LinYujin <devishtank@gmail.com>
2023-03-07 10:29:19 +0700

    Part 2 solution from calhoun

--- quiz.go
+++ quiz.go
@@ -6,10 +6,12 @@ import (
 	"encoding/csv"
 	"fmt"
 	"strings"
+	"time"
 )
 
 func main(){
 	csvfilename := flag.String("csv","problems.csv","CSV file which contains question and answer separated by comma")
+	timeLimit := flag.Int("limit",30,"Duration of time limitation")
 	flag.Parse()
 
 	file,err := os.Open(*csvfilename)
@@ -22,18 +24,28 @@ func main(){
 
 	problems := parseLines(lines)
 
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
+
 	correct := 0
 	for i, p := range problems {
 		fmt.Printf("Problem #%d : %s\n", i+1, p.q)
-		var answer string
-		fmt.Scanf("%s\n",&answer)
-		if answer == p.a{
-			fmt.Printf("Correct\n")
-			correct++
+		answerCh := make(chan string)
+		go func(){
+			var answer string
+			fmt.Scanf("%s\n",&answer)
+			answerCh <- answer
+		}()
+		select {
+		case <- timer.C:
+			fmt.Printf("You answered %d correct answers out of %d problems",correct, len(problems))
+			return
+		case answer := <- answerCh:
+			if answer == p.a{
+				fmt.Printf("Correct\n")
+				correct++
+			}
 		}
 	}
-
-	fmt.Printf("You answered %d correct answers out of %d problems",correct, len(problems))
 }
 
 func check(e error) {
